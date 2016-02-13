e5203d217d9e0f1069eaa831aea14d298ca54808
Louis DeLosSantos <ldelossa.ld@gmai.com>
2016-02-13 15:33:36 -0500

    fab file included

---
+++ kvm_lan/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import sudo, settings, run
+
+def update_dns(hostname, ip):
+    with settings(user='vagrant', password='vagrant'):
+        sudo('echo "{}.kvm.lan.      IN      A      {}" >> /srv/salt/files/bind/kvm.lan.zone'.format(hostname, ip))
+        sudo('salt "dns*" state.highstate')
+
+def salt_key_accept():
+    with settings(user='vagrant', password='vagrant'):
+        sudo('salt-key -y -A')
+
+def salt_highstate(hostname):
+    with settings(user='vagrant', password='vagrant'):
+        sudo('salt "{}*" state.highstate'.format(hostname))
