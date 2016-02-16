53569fcb0d7c9f946a3cf427c83ab7648077b1d4
Louis DeLosSantos <ldelossa.ld@gmai.com>
2016-02-16 01:24:24 -0500

    "changed"

--- kvm_lan/fabfile.py
+++ kvm_lan/fabfile.py
@@ -10,5 +10,5 @@ def salt_key_accept():
         sudo('salt-key -y -A')
 
 def salt_highstate(hostname):
-    with settings(user='vagrant', password='vagrant'):
+    with settings(user='vagrant', password='vagrant', timeout=300):
         sudo('salt "{}*" state.highstate'.format(hostname))
